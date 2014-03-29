d714170c11060512ba77b01c6badf97ceaa2f7c1
Aleksandr Zelenin <aleksandr@zelenin.me>
2014-03-30 03:29:04 +0400

    psr-2

--- Sms.php
+++ Sms.php
@@ -1,38 +1,37 @@
 <?php
-
 namespace Zelenin;
 
 use yii\base\Component;
 
 class Sms extends Component
 {
-	private $sms;
-	public $api_id;
-	public $login;
-	public $password;
+    private $sms;
+    public $api_id;
+    public $login;
+    public $password;
 
-	public function init()
-	{
-		if ( !empty( $this->login ) && !empty( $this->password ) ) {
-			$this->sms = new smsru( $this->api_id, $this->login, $this->password );
-		} else {
-			$this->sms = new smsru( $this->api_id );
-		}
-		parent::init();
-	}
+    public function init()
+    {
+        if (!empty($this->login) && !empty($this->password)) {
+            $this->sms = new smsru($this->api_id, $this->login, $this->password);
+        } else {
+            $this->sms = new smsru($this->api_id);
+        }
+        parent::init();
+    }
 
-	/**
-	 * @param string $name
-	 * @param array  $parameters
-	 *
-	 * @return mixed
-	 */
-	public function __call( $name, $parameters )
-	{
-		if ( method_exists( $this->sms, $name ) ) {
-			return call_user_func_array( array( $this->sms, $name ), $parameters );
-		} else {
-			return call_user_func_array( array( $this, $name ), $parameters );
-		}
-	}
+    /**
+     * @param string $name
+     * @param array $parameters
+     *
+     * @return mixed
+     */
+    public function __call($name, $parameters)
+    {
+        if (method_exists($this->sms, $name)) {
+            return call_user_func_array(array($this->sms, $name), $parameters);
+        } else {
+            return call_user_func_array(array($this, $name), $parameters);
+        }
+    }
 }
